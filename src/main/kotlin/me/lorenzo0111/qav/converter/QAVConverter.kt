c53c984f7ed6006911435a4b88ef2659d2bee2b4
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2022-02-10 19:25:30 +0100

    Added a modifier and a function to restore backups

--- src/main/kotlin/me/lorenzo0111/qav/converter/QAVConverter.kt
+++ src/main/kotlin/me/lorenzo0111/qav/converter/QAVConverter.kt
@@ -17,7 +17,7 @@ class QAVConverter(private val folder: File) {
     private val provider: ConfigurationProvider = ConfigurationProvider.getProvider(YamlConfiguration::class.java)
 
     @Suppress("UnstableApiUsage")
-    fun convert(newY: Double): Boolean {
+    fun convert(Ymodifier: Double): Boolean {
         val start = Instant.now()
         if (!folder.exists()) {
             logger.error("Folder does not exist")
@@ -45,7 +45,7 @@ class QAVConverter(private val folder: File) {
                     Files.copy(file, backup)
 
                 val config = provider.load(file)
-                config.set("driverseat.Offset.y", newY)
+                config.set("driverseat.Offset.y", config.getDouble("driverseat.Offset.y") - Ymodifier)
 
                 provider.save(config, file)
                 converted++
@@ -64,20 +64,59 @@ class QAVConverter(private val folder: File) {
         return true
     }
 
+    fun restore() {
+        val start = Instant.now()
+        if (!folder.exists()) {
+            logger.error("Folder does not exist")
+            return
+        }
+
+        if (!folder.isDirectory) {
+            logger.error("Folder is not a directory")
+            return
+        }
+
+        val files = folder.listFiles() ?: return
+        var restored = 0
+
+        for (file in files) {
+            if (file.isDirectory) continue
+            if (!file.name.endsWith(".bak")) continue
+
+            val substitute = File(file.absolutePath.replace(".bak", ""))
+            if (substitute.exists()) substitute.delete()
+            file.renameTo(substitute)
+
+            restored++
+        }
+
+        val end = Instant.now()
+        logger.info("Restored $restored files in ${Duration.between(start, end).toMillis()}ms")
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            if (args.isEmpty()) {
-                System.err.println("Usage: java -jar qav-converter.jar <folder> [newY]")
+            if (args.size < 2) {
+                System.err.println("Usage: java -jar qav-converter.jar <folder> [Y modifier]")
                 exitProcess(1)
             }
 
-            val converter = QAVConverter(File(args[0]))
-            if (args.size > 1) {
-                converter.convert(args[1].toDoubleOrNull() ?: -1.0)
+            val converter = QAVConverter(File(args[1]))
+
+            if (args[0].equals("convert", ignoreCase = true)) {
+                if (args.size > 2) {
+                    converter.convert(args[2].toDoubleOrNull() ?: 1.9)
+                } else {
+                    converter.convert(1.9)
+                }
+            } else if (args[0].equals("restore", ignoreCase = true)) {
+                converter.restore()
             } else {
-                converter.convert(-1.0)
+                System.err.println("Usage: java -jar qav-converter.jar <convert/restore> <folder> [Y modifier]")
+                exitProcess(1)
             }
+
         }
     }
 }
